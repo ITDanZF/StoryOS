3499271d604ab67b7ffdca7b0f771474a30735ed
jinyuhe <1144565385@qq.com>
2026-06-05 23:43:49 +0800

    feat:新增状态管理库，组件库，以及样式库

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-// https://vitejs.dev/config
-export default defineConfig({
-    plugins: [react()],
+import path from 'path';
+
+export default defineConfig(async () => {
+    const { default: tailwindcss } = await import('@tailwindcss/vite');
+    return {
+        plugins: [react(), tailwindcss()],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+    };
 });
